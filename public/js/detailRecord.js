2ef9cb58b4a4e8f640b5a6bec998400e8d75f6a5
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2022-09-09 09:39:46 +0700

    add new view:

--- public/js/detailRecord.js
+++ public/js/detailRecord.js
@@ -2,16 +2,33 @@ const socket = io();
 const kebutuhanAir = document.querySelector(".kebutuhanAir");
 const kecepatanAir = document.querySelector(".kecepatanAir");
 const ketinggianAir = document.querySelector(".ketinggianAir");
+const lastUpdate = document.querySelector("#lastUpdate");
 
 console.log("HELLO");
 socket.on("connect", () => {
     console.log(socket.id);
 });
+
+const dateTimeFormat = (d) => {
+    const date = new Date(d);
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    return new Intl.DateTimeFormat("id-ID", options).format(date);
+};
+
+lastUpdate.textContent = dateTimeFormat(Date.now());
+
 const listener = (eventName, ...args) => {
     const { waterDepth, waterFlow, soilHumidity } = args[0];
-    kebutuhanAir.textContent = soilHumidity;
+    kebutuhanAir.textContent =
+        soilHumidity === "1" ? "Tidak memerlukan" : "Memerlukan Air";
     kecepatanAir.textContent = waterFlow;
     ketinggianAir.textContent = waterDepth;
+    console.log(args[0]);
 };
 socket.on("record", (data) => {
     console.log(`connect_error due to`);
